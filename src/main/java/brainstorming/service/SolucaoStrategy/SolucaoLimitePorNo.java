6fc00e0a4e97112c509e7bec77811a8e3982e327
Douglas Santos <Douglas Santos@LAPTOP-SNGDI9TS>
2019-06-09 19:54:03 -0300

    Adicionada as strategy MaisVotadaPorNo e LimitePorNo(n de solução é definido pelo ADM, e corrigido um bug na pag ShowSolicitacoes.html

--- src/main/java/brainstorming/service/SolucaoStrategy/SolucaoLimitePorNo.java
+++ src/main/java/brainstorming/service/SolucaoStrategy/SolucaoLimitePorNo.java
@@ -1,14 +1,41 @@
 package brainstorming.service.SolucaoStrategy;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import brainstorming.model.Ideia;
 import brainstorming.model.Sessao;
+import brainstorming.model.estrutura.No;
 
 public class SolucaoLimitePorNo implements SolucaoStrategy{
-
+	
+	// Precisa de uma entrada do numero de ideia por nó
 	public List<Ideia> calcSolucao(Sessao sessao) {
 		
-		return null;
+		// Valor definido pelo ADM
+		int numSolporNo = 3;
+		List<Ideia> sol = new ArrayList<Ideia>();
+		List<Ideia> aux = new ArrayList<Ideia>();
+		
+		for (No no : sessao.getEstrutura().getNos()) {
+			aux.addAll(no.getIdeias());
+			
+			Collections.sort(aux);
+			
+			if(aux.size() > numSolporNo) {
+				sol.addAll(aux.subList(0, numSolporNo -1));
+			}else {
+				sol.addAll(aux);
+			}
+			
+			aux.clear();
+			numSolporNo = 3;
+		}
+		
+		return sol;
+		
 	}
 }
+
+
